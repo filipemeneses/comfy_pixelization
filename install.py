39e46db1d394d8e27a1f092e5637c3eb9033a456
validaq <validaq@gmail.com>
2025-06-12 05:49:00 +0200

    Update the install script to set up the submodule upon installation

--- install.py
+++ install.py
@@ -1,7 +1,14 @@
 import contextlib
 import os
+import subprocess
 
 path = os.path.dirname(os.path.realpath(__file__))
+
+def git(*args):
+    return subprocess.check_call(["git", *list(args)])
+
+# Install Pixelization submodule
+git("submodule", "update", "--init")
 
 # we remove __init__ because it breaks BLIP - takes over the directory named models which BLIP also uses.
 with contextlib.suppress(OSError):
