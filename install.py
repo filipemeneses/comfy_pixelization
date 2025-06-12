b97acbe89cf79f7d42084c669a21857fdb3c5cbe
validaq <validaq@gmail.com>
2025-06-12 05:48:59 +0200

    Remove unnecessary git commands from install script

--- install.py
+++ install.py
@@ -1,19 +1,8 @@
 import contextlib
 import os
-import subprocess
-
-
-def git(*args):
-    return subprocess.check_call(["git", *list(args)])
-
 
 path = os.path.dirname(os.path.realpath(__file__))
-repo_path = os.path.join(path, "pixelization")
-
-git("clone", "https://github.com/WuZongWei6/Pixelization.git", repo_path)
-os.chdir(os.path.join(path, "pixelization"))
-git("checkout", "b7142536da3a9348794bce260c10e465b8bebcb8")
 
 # we remove __init__ because it breaks BLIP - takes over the directory named models which BLIP also uses.
 with contextlib.suppress(OSError):
-    os.remove(os.path.join(path, "pixelization", "models", "__init__.py"))
+    os.remove(os.path.join(path, "Pixelization", "models", "__init__.py"))
